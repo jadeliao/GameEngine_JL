1a147175c1e383c436f8a3fb7b2b966bae246e34
jadeliao <n01446860@humbermail.ca>
2022-11-17 13:21:33 -0500

    Small Fix in A*

--- GameEngine/Graph.cpp
+++ GameEngine/Graph.cpp
@@ -66,73 +66,84 @@ std::vector<int> Graph::neighbours(int fromNode)
 }
 
 std::vector<int> Graph::Dijkstra(int startNode, int goalNode) {
-	//declare helper variables
+	//Declare helper variables
 	float new_cost;
 	int current = startNode;
-	//declare current NodeAndPriority
+	//Declare current NodeAndPriority
 	Ref<NodeAndPriority> currentNodeAndPriority;
 	currentNodeAndPriority = std::make_shared<NodeAndPriority>(node[startNode], 0.0f);
-	//set up priority queue for frontier of nodes
-
+	//Set up priority queue for frontier of nodes
 	std::priority_queue<Ref<NodeAndPriority>, deque<Ref<NodeAndPriority>>, ComparePriority> frontier;
 	frontier.push(currentNodeAndPriority);
 
-	//track solution path
-	vector<int> came_from;
-	came_from.resize(numNodes());
+	//Track solution path
+	map<int, int> came_from;
 
-	//store cost
+	//Store cost
 	map<int, float> cost_so_far;
 	cost_so_far[startNode] = 0.0f;
-
-	//TODO implement the algorithm
-	//start looping through frontier
+	//Add the start node to came_from
+	came_from[startNode] = 0;
+	//Start looping through frontier
 	while (!frontier.empty()) {
-		//get the node from the top of  the frontier and pop it off
+		//Get the node from the top of  the frontier and pop it off
 		currentNodeAndPriority = frontier.top();
+		current = currentNodeAndPriority->node->getLabel();
 		frontier.pop();
-		//if its the goal, then break out of the loop
+		//If reaches the goal, then break out of the loop
 		if (currentNodeAndPriority->node == node[goalNode]) {
 			break;
 		}
-		//for the neibours of current node
+		//Loop through the neibours of current node
 		vector<int> neighbourList = neighbours(current);
 		for (int next : neighbourList) {
-			//calculate new cost
+			//Calculate new cost
 			float new_cost = cost_so_far[current] + cost[current][next];
-			//if neighbour is not in cost_so_far or new cost is lower
-			if (cost_so_far.find(next) != cost_so_far.end() || new_cost < cost_so_far[next]) {
+			if (cost_so_far.find(next) == cost_so_far.end() ||
+				new_cost < cost_so_far[next]) {
 				cost_so_far[next] = new_cost;
-				currentNodeAndPriority->priority = new_cost;
-				frontier.push(currentNodeAndPriority);
+				float priority = new_cost;
+				Ref<NodeAndPriority> tempNodeAndPriority =
+					std::make_shared<NodeAndPriority>(node[next], priority);
+				frontier.push(tempNodeAndPriority);
 				came_from[next] = current;
-			}	
+			}
 		}
 	}
-	return came_from;
+	//Reconstruct the path
+	std::vector<int> path;
+	current = goalNode;
+	while (current != startNode) {
+		path.push_back(current);
+		current = came_from[current];
+	}
+	path.push_back(startNode);
+	std::reverse(path.begin(), path.end());
+	return path;
 	
 }
 
 //Passing in label
 std::vector<int> Graph::AStar(int startNode, int goalNode)
 {
-	//declare helper variables
+	//Declare helper variables
 	float new_cost;
 	int current = startNode;
-	//declare current NodeAndPriority
+	//Declare current NodeAndPriority
 	Ref<NodeAndPriority> currentNodeAndPriority;
 	currentNodeAndPriority = std::make_shared<NodeAndPriority>(node[startNode], 0.0f);
-	//set up priority queue for frontier of nodes
+	//Set up priority queue for frontier of nodes
 	std::priority_queue<Ref<NodeAndPriority>, deque<Ref<NodeAndPriority>>, ComparePriority> frontier;
 	frontier.push(currentNodeAndPriority);
 
-	//track solution path
+	//Track solution path
 	map<int, int> came_from;
-	//came_from.resize(numNodes());
-	//store cost
+
+	//Store cost
 	map<int, float> cost_so_far;
 	cost_so_far[startNode] = 0.0f;
 	came_from[startNode] = 0;
+
 	//Start looping through frontier
 	while (!frontier.empty()) {
 		//Get the node from the top of  the frontier and pop it off
@@ -151,15 +162,16 @@ std::vector<int> Graph::AStar(int startNode, int goalNode)
 			if (cost_so_far.find(next) == cost_so_far.end() || 
 				new_cost < cost_so_far[next]) {
 				cost_so_far[next] = new_cost;
-				//Use Manhattan Heuristic for 4 direction movement  + manhattanHeuristic(node[next], node[goalNode])
+				//Use Manhattan Heuristic for 4 direction movement  
+				float priority = new_cost + manhattanHeuristic(node[next], node[goalNode]);
 				Ref<NodeAndPriority> tempNodeAndPriority = 
-					std::make_shared<NodeAndPriority>(node[next], new_cost);
+					std::make_shared<NodeAndPriority>(node[next], priority);
 				frontier.push(tempNodeAndPriority);
-				//Only push back when the current node does not exist in list
 				came_from[next] = current;
 			}
 		}
 	}
+	//Reconstruct the path
 	std::vector<int> path;
 	current = goalNode;
 	while (current != startNode) {
